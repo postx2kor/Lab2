03294cdeae1b23e531ba86ecf2a63a547150285f
JHS <ikon10044@gmail.com>
2024-06-13 14:57:01 +0000

    modified func

--- main.cpp
+++ main.cpp
@@ -3,11 +3,30 @@
 
 using namespace std;
 
+
 int N,M;
 vector<int> vec;
 
 void func(int cnt) {
-
+    if(cnt == M){
+	    for(auto i : vec) cout << i << ' ';
+	    cout << endl;
+	    return;
+    }
+    if(vec.empty()){
+	    for(int i=1; i<=N; i++){
+		    vec.push_back(i);
+		    func(1);
+		    vec.pop_back();
+	    }
+    }
+    else {
+	for(int i = vec.back()+1; i<=N; i++){
+	   vec.push_back(i);
+	   func(cnt+1);
+	   vec.pop_back();
+    	}
+    }
 
 
 
